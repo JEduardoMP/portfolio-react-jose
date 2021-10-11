068711cbc43a497591318f99fb953d4fe34a2086
Jose Eduardo <eduardomarque2009@gmail.com>
2021-10-11 16:04:40 -0500

    some changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,13 +4,13 @@ import mypicture from "../assets/mypicture.jpg"
 
 const Navbar = () => {
     return(
-        <header className="flex aling-center my-8">
+        <header className="flex items-center my-8">
             <div className="flex">
                 <img src={mypicture} alt="my photo" className="rounded-full h-24"/>
             </div>
-            <div>
-                <h2 className="font-bold text-2xl md:text-4x1">Jose E. Marquez</h2>
-                <ul className="font-bold text-sm mt-2 flex justify-between">
+            <div className="flex flex-col justify-end ml-4 md:w-6/12">
+                <h2 className="font-bold text-2xl md:text-2x1">Jose E. Marquez</h2>
+                <ul className="font-bold text-sm mt-2 flex justify-between lg:text-lg">
                     <li>
                         <NavLink exact to="/" activeClassName="text-gold">
                             Home
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -7,24 +7,24 @@ const Contact = () => {
             <p className="font-light py-2 mb-10 lg:text-left">If you’d like to chat about a project or any work, please let me know.</p>
 
             <form action="" className="flex justify-center aling-center flex-col md:grid md:grid-cols-2 md:gap-3 lg:grid-cols-3">
-                <div className="w-full flex flex-col text-center mb-8">
+                <div className="w-full flex flex-col text-center mb-8 lg:text-left">
                     <label htmlFor="">Name <span className="text-red font-bold w-full">*</span></label>
-                    <input type="text" name="" id=""  className="p-2 text-center" required/>
+                    <input type="text" name="name" id=""  className="p-2 text-center lg:text-left" required/>
                 </div>
 
-                <div className="w-full flex flex-col text-center mb-8">
+                <div className="w-full flex flex-col text-center mb-8 lg:text-left">
                         <label htmlFor="">Email <span className="text-red font-bold">*</span></label>
-                        <input type="email" name="" id=""  className="p-2 text-center" required/>
+                        <input type="email" name="email" id=""  className="p-2 text-center lg:text-left" required/>
                 </div>
 
-                <div className="w-full flex flex-col text-center mb-8">
+                <div className="w-full flex flex-col text-center mb-8 lg:text-left">
                     <label htmlFor="">Subject <span className="text-red font-bold">*</span></label>
-                    <input type="text" name="Subject" id=""  className="p-2 text-center" required/>
+                    <input type="text" name="Subject" id=""  className="p-2 text-center lg:text-left" required/>
                 </div>
 
-                <div className="w-full flex flex-col text-center md:col-start-1 md:col-end-3 lg:col-start-1 lg:col-end-4">
+                <div className="w-full flex flex-col text-center md:col-start-1 md:col-end-3 lg:col-start-1 lg:col-end-4 lg:text-left">
                     <label htmlFor="">Message <span className="text-red font-bold">*</span></label>
-                    <textarea type="text" name="" id=""  className="p-8"/>
+                    <textarea type="text" name="message" id=""  className="p-8"/>
                 </div>
                 <input type="submit" name="Submit" id="" className="mb-8 p-4 bg-gold hover:bg-yellow md:col-start-1 col-end-3 lg:col-start-3 lg:col-end-4 lg:w-5/6 lg:flex-row-reverse "/>
             </form>
