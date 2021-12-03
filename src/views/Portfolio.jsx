b9403572abb6c94c8fff295cd7ea0587b3d99fc2
Jose <eduardomarque2009@mail.com>
2021-12-03 13:39:36 -0600

    trivia photo

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -1,3 +1,5 @@
+import triviaImg from '../assets/Trivia.jpg'
+
 const Portfolio = () => {
     return(
         <section className="text-gray-600 body-font py-24" id="portfolio">
@@ -78,7 +80,7 @@ const Portfolio = () => {
                 {/* Third tab */}
                 <div className="p-4 md:w-1/3">
                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog" />
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={triviaImg} alt="Trivia" />
                     <div className="p-6">
                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Trivia Vanilla JS</h1>
